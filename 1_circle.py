4c3e3badc9cb9f2d67e0f2ffc7065c2a9f88945f
vitarafavich <v.rafaevich@g.nsu.ru>
2025-03-23 17:00:55 +0700

    first commit

---
+++ 1_circle.py
@@ -0,0 +1,53 @@
+"""
+task 1
+Опишите класс окружности Circle. 
+Класс должен содержать атрибут класса all_circles со списком всех экземпляров этого класса. 
+Экземпляр класса может быть инициализирован необязательным параметром радиусом. 
+В случае, если параметр не задан, экземпляр создается с единичным радиусом. 
+Класс должен иметь атрибут класса рі (со значением 3.1415). 
+Метод area() должен возвращать площадь круга. 
+Так же, опишите статический метод total_area(), который возвращает суммарную площадь всех экземпляров класса Circle.
+"""
+
+
+class Circle:
+    """
+    Class representing a circle
+    """
+    all_circles = []
+    pi = 3.1415
+
+    def __init__(self, radius=1):
+        self.radius = radius
+        Circle.all_circles.append(self)
+
+    def area(self):
+        """
+        Method area: counting the area of the circle
+        """
+        return Circle.pi * self.radius ** 2
+
+    def __str__(self):
+        return f'{self.radius}'
+
+    def __len__(self):
+        return len(self.all_circles)
+
+    @staticmethod
+    def total_area():
+        total = 0
+        for circle in Circle.all_circles:
+            total += circle.area()
+        return total
+
+
+c1 = Circle()
+c2 = Circle(7)
+c3 = Circle(5)
+print(c2.area())
+print(c3)
+print(Circle.pi)
+print(Circle.all_circles) #problems
+print(Circle.total_area())
+print(len(c3.__class__.all_circles))
+
