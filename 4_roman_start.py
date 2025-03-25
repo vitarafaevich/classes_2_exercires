958d48e9ab1dfa303c6384d685767cef281f83b6
vitarafavich <v.rafaevich@g.nsu.ru>
2025-03-26 01:04:49 +0700

    DEBUGGING COMMIT!!!!!!!!!!!!!!!!!

--- 4_roman_start.py
+++ 4_roman_start.py
@@ -4,7 +4,8 @@ class RomanNumber:
     :method is_roman(value):
     :method decimal_number(self):
     """
-    decimal_dict = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    decimal_dict = {'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9,
+                    'V': 5, 'IV': 4, 'I': 1}
     decimal_couple = {'CM': 900, 'CD': 400, 'XC': 90, 'XL': 40, 'IX': 9, 'IV': 4}
 
     def __init__(self, number):
